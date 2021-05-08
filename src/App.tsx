dae3c2f574b63687000f3a43e5e7a48f0fae1452
ZayaByghead <ZayaByghead@gmail.com>
2021-05-08 09:01:42 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Layout} from 'antd'
+import 'antd/dist/antd.css'
+import './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NavBar from './components/NavBar'
+import Content from './components/Content'
+
+
+export default function App() {
+    return (
+        <Layout>
+            <Layout.Header
+                className='background-white'
+                style={{padding: 0}}
+            >
+                <NavBar/>
+            </Layout.Header>
+
+            <Layout.Content>
+                <Content/>
+            </Layout.Content>
+        </Layout>
+    )
 }
 
-export default App;
